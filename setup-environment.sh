02f1854290257c4003ce33cdfe0ef8e9bd6f4868
drandarov <s.drundarov@gmail.com>
2020-05-10 13:43:38 +0300

    feat: Complete setup-environment script
    - add function to validate database connection
    - add function to setup Laravel .env config
    - generate laravel application key and migrations as finishing touches

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-function checkAndSetPorts () {
+function checkAndSetPorts() {
     local name=$1
     local port=$2
 
     echo "Checking for free $name port..."
     while :; do
-        if ! ss -tlwn | grep ":$port" > /dev/null; then
+        if ! ss -tlwn | grep ":$port" >/dev/null; then
             echo "$name $port is free"
             printf -v "${name}" "%s" "$port" # Sets the value of variable with the same name as the value of $name to the free port
             echo "Using $port as $name port"
@@ -15,6 +15,61 @@ function checkAndSetPorts () {
             port=$((port + 1))
         fi
     done
+}
+
+function checkDbConnection() {
+    local counter=1
+    local username=$1
+    local password=$2
+
+    echo "Trying connection to a max of 20 tries"
+    while [ "$counter" -lt "21" ]; do
+        echo "Attempt $counter"
+
+        if docker-compose exec digi-panel-db mysql -u "$username" -p"$password" -e "SHOW DATABASES;" | grep -q 'digi-panel'; then
+            echo "digi-panel database exists"
+            echo "Validating connection to digi-panel"
+
+            if docker-compose exec digi-panel-db mysql -u "$username" -p"$password" -e "SHOW TABLES FROM \`digi-panel\`;"; then
+                break
+            else
+                echo "Could not connect to the digi-panel database..."
+                exit
+            fi
+        fi
+
+        echo "Could not connect. Retrying..."
+        counter=$((counter + 1))
+    done
+
+    if [ "$counter" -ge "20" ]; then
+        echo "Could not connect to MariaDB..."
+        exit
+    fi
+
+    echo "Connection successful"
+}
+
+function setupEnvFile() {
+    local -A params=(
+        ["{{APP_NAME}}"]="\"Digi Panel\""
+        ["{{WEB_PRIMARY_PORT}}"]=$httpPort
+        ["{{DB_HOST}}"]="digi-panel-db"
+        ["{{DB_PORT}}"]=$dbPort
+        ["{{DB_USERNAME}}"]=$1
+        ["{{DB_PASSWORD}}"]=$dbPassword
+    )
+
+    echo "Copying .env file..."
+    rsync ./.env.example ./.env
+
+    echo "Setting up placeholder parameters..."
+    for index in "${!params[@]}"; do
+        echo "Setting $index to ${params[$index]}"
+        sed -i "s/$index/${params[$index]}/g" .env
+    done
+
+    echo "File setup complete"
 }
 
 echo "Beginning local environment setup..."
@@ -45,10 +100,10 @@ echo "[Press enter to leave blank]"
 echo
 read -rsp "Password: " rootPassword
 sed -i "s/{{DB_ROOT_PASSWORD}}/$rootPassword/g" docker-compose.yml
+echo
 echo "Password set successfully"
 
 echo "Running docker-compose up..."
-echo
 echo
 echo
 docker-compose up -d
@@ -60,9 +115,9 @@ else
     exit
 fi
 
+echo "Installing dependencies with composer install..."
 if [ -d "./vendor/" ]; then
-    echo "Vendor folder already exists..."
-    echo "If you want to exec more composer commands you must do so via the container"
+    echo "Vendor folder already exists. Skipping..."
 else
     echo "Vendor folder not found. Executing composer install..."
     docker-compose exec --user www digi-panel-app /usr/local/bin/composer install
@@ -75,17 +130,33 @@ else
 fi
 
 echo "Checking database is successfully created..."
-if docker-compose exec digi-panel-app mysql -u root -p rootPassword -e "SHOW DATABASES;" | grep "laravel"; then
-    echo "Database exists..."
-else
-    echo "Database could not be found!"
-    exit
-fi
+checkDbConnection "root" "$rootPassword"
 
 echo "Creating separate database user for the application..."
 echo "Please set a password for the user"
 echo "[Press enter to leave blank]"
 echo
 read -rsp "Password: " dbPassword
-docker-compose exec digi-panel-app mysql -u root -p rootPassword -e "GRANT ALL ON laravel.* TO 'digiuser'@'%' IDENTIFIED BY '$dbPassword';"
-docker-compose exec digi-panel-app mysql -u root -p rootPassword -e "FLUSH PRIVILEGES;"
+docker-compose exec digi-panel-db mysql -u root -p"$rootPassword" -e "GRANT ALL ON \`digi-panel\`.* TO 'digiuser'@'%' IDENTIFIED BY '$dbPassword';"
+docker-compose exec digi-panel-db mysql -u root -p"$rootPassword" -e "FLUSH PRIVILEGES;"
+echo
+echo "Verifying database connection with new user..."
+checkDbConnection "digiuser" "$dbPassword"
+
+echo "Setting up .env file"
+setupEnvFile "digiuser"
+
+echo "Executing finishing touches..."
+echo "Generating laravel key..."
+docker-compose exec digi-panel-app php artisan key:generate
+echo "Running laravel migrate..."
+docker-compose exec digi-panel-app php artisan migrate
+echo "Validating application db connection"
+if echo "DB::connection()->getPdo();" | docker-compose exec -T digi-panel-app php artisan tinker; then # Disable TTY allocation otherwise error is thrown
+    echo "Laravel DB connection successful"
+else
+    echo "Could not establish connection"
+    exit
+fi
+
+echo "Local environment setup complete"
