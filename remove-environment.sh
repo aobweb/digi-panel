7f370d0f20cd2a775949d82e798d884789d10ad2
drandarov <s.drundarov@gmail.com>
2020-05-11 07:20:34 +0300

    feat: Add script to remove docker environment

---
+++ remove-environment.sh
@@ -0,0 +1,33 @@
+echo "Checking if environment exists..."
+if ! docker ps -a | grep -i "digi-panel-.*"; then
+    echo "No digi panel containers found. Exiting..."
+    exit
+fi
+
+echo "Stopping and removing containers..."
+if docker container stop digi-panel-app digi-panel-web digi-panel-db | docker container rm; then
+    echo "Validating operation success..."
+    if ! docker ps -a | grep "digi-panel-.*"; then
+        echo "Containers successfully removed"
+    fi
+else
+    echo "Could not remove digi panel containers..."
+    exit;
+fi
+
+if docker volume ls | grep -i "digi-panel.*"; then
+    echo "Would you also like to remove the database volume?"
+    read -rp "[y/n] " input
+
+    if echo "$input" | grep -i ".*y.*"; then
+        echo "Removing database volume..."
+        if docker volume rm digi-panel_dbdata; then
+            echo "Volume successfully removed"
+        else
+            echo "Could not remove volume..."
+            exit
+        fi
+    fi
+fi
+
+echo "Operations complete"
